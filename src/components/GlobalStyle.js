3c4f6d9cbe44023f7648840e90ea12f633b8cd69
SedanaYoga <9g.yoga27@gmail.com>
2021-02-07 18:10:06 +0700

    Adding reuseable styles components and Inter fontfam

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -9,11 +9,13 @@ const GlobalStyle = createGlobalStyle`
 
   body {
     background: #1b1b1b;
+    font-family: 'Inter', sans-serif;
   }
 
   button {
     font-weight: bold;
     font-size: 1.1.rem;
+    font-family: 'Inter', sans-serif;
     cursor: pointer;
     padding: 1rem 2rem;
     border: 3px solid #23d997;
@@ -43,10 +45,10 @@ const GlobalStyle = createGlobalStyle`
     font-size: 1.1.rem;
   }
   p{
-     padding: 3rem 0rem;
-     color: #ccc;
-     font-size: 1.4rem;
-     line-height: 150%;
+    padding: 3rem 0rem;
+    color: #ccc;
+    font-size: 1.4rem;
+    line-height: 150%;
   }
 `;
 
