3ccb1b96ba5f8253856942953c08d81a344dd2d5
SedanaYoga <9g.yoga27@gmail.com>
2021-02-07 19:25:55 +0700

    Created ContactUs, OurWorks page and added react-router-dom to project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 // Import Pages
 import AboutUs from "./pages/AboutUs";
+import OurWorks from "./pages/OurWorks";
+import ContactUs from "./pages/ContactUs";
 // Import Global Style
 import GlobalStyle from "./components/GlobalStyle";
 // Import Components
 import Nav from "./components/Nav";
+//Route
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWorks />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/OurWorks.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function OurWorks() {
+  return (
+    <div>
+      <h1>Our Work</h1>
+    </div>
+  );
+}
+
+export default OurWorks;
